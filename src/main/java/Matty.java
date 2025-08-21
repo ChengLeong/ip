fe1b32e0e449c8eb3ee0af2ecd55182004f9eb20
ChengLeong <e1398428@u.nus.edu>
2025-08-22 00:17:29 +0800

    Level 2

--- src/main/java/Matty.java
+++ src/main/java/Matty.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Matty {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> task = new ArrayList<>();
         System.out.println("Hello! I'm Matty\n" +
                 "What can I do for you?\n");
 
@@ -11,8 +14,13 @@ public class Matty {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")){
+                for (int i = 0; i < task.size(); i++) {
+                    System.out.println( (i + 1) + "." + task.get(i));
+                }
             } else {
-                System.out.println(" " + input);
+                task.add(input);
+                System.out.println("added: " + input);
             }
         }
         sc.close();
