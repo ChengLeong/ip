a4bc567409b19011538d56050264127688a81cb1
ChengLeong <e1398428@u.nus.edu>
2025-08-31 10:41:08 +0800

    A-JUnit

---
+++ src/test/java/matty/TaskTest.java
@@ -0,0 +1,27 @@
+package matty;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+
+    @Test
+    public void markAsDone_success() {
+        Task t = new Task("read book");
+        t.markAsDone();
+        assertTrue(t.isDone);
+    }
+
+    @Test
+    public void toString_notDone_success() {
+        Task t = new Task("read book");
+        assertEquals("[ ] read book", t.toString());
+    }
+
+    @Test
+    public void toString_done_success() {
+        Task t = new Task("read book");
+        t.markAsDone();
+        assertEquals("[X] read book", t.toString());
+    }
+}
